b8f96870da8df4b3c3226bc87e4354736037318a
Luigi Cocconcelli <k-github@rete.us>
2020-04-25 14:17:24 +0200

    fix hang on reboot

--- distro-bionic/fn_c2_install_openssh.sh
+++ distro-bionic/fn_c2_install_openssh.sh
@@ -4,31 +4,35 @@
 
 install_openssh() {
 	# $1: port - strictly in numerical range
-	local PRT=$(port_validate ${1})
+	local p=$(port_validate ${1})
 
 	# configure SSH server arguments
 	sed -ri /etc/ssh/sshd_config \
-		-e "s|^#?Port.*|Port ${PRT}|" \
+		-e "s|^#?Port.*|Port ${p}|" \
 		-e 's|^#?(PasswordAuthentication).*|\1 no|' \
 		-e 's|^#?(PermitRootLogin).*|\1 without-password|' \
 		-e 's|^#?(RSAAuthentication).*|\1 yes|' \
 		-e 's|^#?(PubkeyAuthentication).*|\1 yes|'
 
 	# mitigating ssh hang on reboot on systemd capables OSes
-	[ -s /etc/systemd/system/ssh-user-sessions.service ] || {
+	[ -s /etc/systemd/system/ssh-session-cleanup.service ] || {
 		msg_info "Mitigating the SSH hang on reboot's problem"
-		copy_to /etc/systemd/system ssh/ssh-user-sessions.service
-		cmd systemctl enable ssh-user-sessions.service
+#		copy_to /etc/systemd/system ssh/ssh-user-sessions.service
+#		cmd systemctl enable ssh-user-sessions.service
+#		cmd systemctl daemon-reload
+		cp /usr/share/doc/openssh-client/examples/ssh-session-cleanup.service /etc/systemd/system/
+		cmd systemctl enable ssh-session-cleanup.service
 		cmd systemctl daemon-reload
+		cmd systemctl start ssh-session-cleanup.service
 	}
 
 	# fix a systemd bug of xenial 16.04
 	# https://askubuntu.com/questions/1109934/ssh-server-stops-working-after-reboot-caused-by-missing-var-run-sshd
-	msg_info "Fixing a little systemd bug that prevent SSHd to start"
-	sed -i 's|/var||' /usr/lib/tmpfiles.d/sshd.conf
+#	msg_info "Fixing a little systemd bug that prevent SSHd to start"
+#	sed -i 's|/var||' /usr/lib/tmpfiles.d/sshd.conf
 
 	# activate on firewall & restart SSH
-	firewall_allow "${PRT}"
+	firewall_allow "${p}"
 	svc_evoke ssh restart
-	msg_info "The SSH server is listening on port: ${PRT}"
+	msg_info "The SSH server is listening on port: ${p}"
 }	# end install_openssh
