8966efaa14253b9411c7f792a4b829ba214ff834
Luigi Cocconcelli <k-github@rete.us>
2020-04-26 10:14:12 +0200

    lowercasing vars

--- distro-bionic/fn_a2_menu_resolv.sh
+++ distro-bionic/fn_a2_menu_resolv.sh
@@ -4,28 +4,28 @@
 # ------------------------------------------------------------------------------
 
 menu_resolv() {
-	local N T="OpenNIC" R=/etc/resolv.conf
-	backup_file ${R}
+	local n t="OpenNIC" r=/etc/resolv.conf
+	backup_file ${r}
 
 	# Getting the nearest OpenNIC servers using their geoip API
-#	N=$(cmd curl -s 'https://api.opennicproject.org/geoip/' | head -3 | awk '{print "nameserver",$1}')
-#	N=$(cmd wget -q4O- 'https://api.opennicproject.org/geoip/?ns&res=4&ipv=4')
+#	n=$(cmd curl -s 'https://api.opennicproject.org/geoip/' | head -3 | awk '{print "nameserver",$1}')
+#	n=$(cmd wget -q4O- 'https://api.opennicproject.org/geoip/?ns&res=4&ipv=4')
 
 	# otherwise set the freenom.world public dns
-	[ -z "${N}" ] && {
-		T="cloudflare + freenom.world"
-		N="search .\noptions timeout:2 rotate\n"
-		N+="nameserver 1.1.1.1      # cloudflare\n"
-		N+="nameserver 80.80.80.80  # freenom.world\n"
-		N+="nameserver 1.0.0.1      # cloudflare\n"
-		N+="nameserver 80.80.81.81  # freenom.world"
+	[ -z "${n}" ] && {
+		t="cloudflare + freenom.world"
+		n="search .\noptions timeout:2 rotate\n"
+		n+="nameserver 1.1.1.1      # cloudflare\n"
+		n+="nameserver 80.80.80.80  # freenom.world\n"
+		n+="nameserver 1.0.0.1      # cloudflare\n"
+		n+="nameserver 80.80.81.81  # freenom.world"
 	}
 
 	# write in /etc/resolv.conf
-	cmd chattr -i ${R}	# allow file modification
-	echo -e "# public dns\n${N}" > ${R}
-	cmd chattr +i ${R}	# disallow file modification
+	cmd chattr -i ${r}	# allow file modification
+	echo -e "# public dns\n${n}" > ${r}
+	cmd chattr +i ${r}	# disallow file modification
 
-	msg_info "Configuration of ${T} public dns completed! Now ${R} has:"
-	sed 's|^|> |' < ${R}
+	msg_info "Configuration of ${t} public dns completed! Now ${r} has:"
+	sed 's|^|> |' < ${r}
 }	# end menu_resolv
