9778e52433dbe16cfc7b44f1215422c41eb6a5bb
Luigi Cocconcelli <k-github@rete.us>
2020-05-02 10:53:53 +0200

    Update fn_c2_install_openssh.sh

--- distro-stretch/fn_c2_install_openssh.sh
+++ distro-stretch/fn_c2_install_openssh.sh
@@ -21,9 +21,12 @@ install_openssh() {
 		msg_info "Mitigating the problem of SSH hangs on reboot"
 		cp /usr/share/doc/openssh-client/examples/${X} /etc/systemd/system/
 #		copy_to /etc/systemd/system ssh/${X}
+		cmd systemctl daemon-reload
 		cmd systemctl enable ${X}
 		cmd systemctl start ${X}
-		cmd systemctl daemon-reload
+		# edit script to catch all sshd demons: shell & winscp
+		sed -ri /usr/lib/openssh/ssh-session-cleanup \
+			-e "s|^(ssh_session_pattern).*|\1='sshd: \S.*@\w+'|"
 	}
 
 	# activate on firewall & restart SSH
