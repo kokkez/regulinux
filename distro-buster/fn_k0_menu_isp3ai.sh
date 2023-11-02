aa28abbd208ddc58a2af5f029577534382872af0
Luigi Cocconcelli <k-github@rete.us>
2023-11-02 18:56:53 +0100

    ispconfig autoinstall

---
+++ distro-buster/fn_k0_menu_isp3ai.sh
@@ -0,0 +1,31 @@
+# ------------------------------------------------------------------------------
+# install ispconfig3 for debian 10 buster in an automatic fashion
+# https://www.howtoforge.com/ispconfig-autoinstall-debian-ubuntu
+# ------------------------------------------------------------------------------
+
+Menu.isp3ai() {
+	# $1: optional webserver type: nginx or apache2
+	HTTP_SERVER="${1:-$HTTP_SERVER}"
+
+	# abort if "Menu.deps" was not executed
+	Deps.performed || return
+
+	# install update-inetd
+	Pkg.requires update-inetd
+
+	# install webserver (nginx or apache2)
+	cmd wget -O - https://get.ispconfig.org | cmd sh -s -- \
+		--debug \
+		--no-dns \
+		--use-unbound \
+		--no-mailman \
+		--use-amavis \
+		--use-nginx \
+		--use-php=5.6,7.4,8.3 \
+		--use-ftp-ports=40110-40210 \
+		--unattended-upgrades \
+		--i-know-what-i-am-doing
+
+#	install_adminer
+#	install_sslcert_selfsigned
+}	# end Menu.isp3ai
