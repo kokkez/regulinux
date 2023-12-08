f0f9c07038a4e7f828980658d4f4e95e83110dab
Luigi Cocconcelli <k-github@rete.us>
2023-12-08 18:21:16 +0100

    Update fn_b8_deploy.sh

--- functions/fn_b8_deploy.sh
+++ functions/fn_b8_deploy.sh
@@ -7,18 +7,22 @@ Menu.deploy() {
 	# $1 - numeric debian version: 10, 11, 12
 	local a g v=${1:-11}
 
-	# detect: address, gateway
+	# detect ip address with subnet mask
 	a="$(cmd ip -br -4 addr show scope global)"
 	a=$(cmd grep -oP '\s+UP\s+\K[\w\.]+' <<< "$a")
+	Msg.info "Detected IP $a..."
+
+	# detect gateway
 	g="$(cmd ip route get 1.1.1.1)"
 	g=$(cmd grep -oP '\s+via\s+\K[\w\.]+' <<< "$g")
+	Msg.info "Detected gateway $g..."
 
 	Msg.info "Preparing to install Debian $v..."
 
 	# save parameters to use once rebooted
 	File.download "https://raw.githubusercontent.com/bohanyang/debi/master/debi.sh" "debi.sh"
 	bash debi.sh --version $v \
-		--ip $i --gateway $g \
+		--ip $a --gateway $g \
 		--dns '1.1.1.1 9.9.9.9 2606:4700:4700::1111 2620:fe::fe' \
 		--user root --password bamalama \
 		--timezone $TIME_ZONE \
