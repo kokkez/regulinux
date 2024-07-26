0cbcffaebf64e63cc4658b5437adf462a6b648ec
Luigi Cocconcelli <k-github@rete.us>
2024-07-26 10:01:35 +0200

    Update fn_b4_reinstall.sh

--- functions/fn_b4_reinstall.sh
+++ functions/fn_b4_reinstall.sh
@@ -3,7 +3,7 @@
 # https://github.com/bohanyang/debi
 # ------------------------------------------------------------------------------
 
-Server.isContainer() {
+Server.notContainer() {
 	# returns 1 (error) if the current system is a virtualized container
 	# container OpenVZ
 	[ -d /proc/vz ] && {
@@ -16,7 +16,7 @@ Server.isContainer() {
 		return 1
 	}
 	return 0
-}	# end Server.isContainer
+}	# end Server.notContainer
 
 
 Menu.reinstall() {
@@ -25,7 +25,7 @@ Menu.reinstall() {
 	local a g v=${1:-11}
 
 	# do checks
-	Server.isContainer && return 1
+	Server.notContainer || return 1
 
 	# start procedure
 	Msg.info "Preparing to install Debian $v..."
