b3cc89acae9860fe06845f93a860feda93f72370
Luigi Cocconcelli <k-github@rete.us>
2025-03-26 20:08:45 +0100

    bugfix fmpro on ubuntu 20 focal

--- distro-focal/fn_u1_menu_fmserver.sh
+++ distro-focal/fn_u1_menu_fmserver.sh
@@ -95,7 +95,8 @@ Menu.fms() {
 #	local u d v=21.0.2.202	# version to install
 	local u d v=21.1.3.305	# version to install
 	d=/opt/FileMaker		# directory root
-	u="https://cloud.italmedia.net/s/nWKTYQfdJmZzEwk/download/fms_${v}_Ubuntu20_amd64.zip"	# directory root
+#	u="https://cloud.italmedia.net/s/nWKTYQfdJmZzEwk/download/fms_${v}_Ubuntu20_amd64.zip"	# directory root
+	u="https://cloud.italmedia.net/s/T8Aiqmb9TzgRByq/download/fms_${v}_Ubuntu20_amd64.zip"	# directory root
 
 	# test if filemaker server is already installed
 	[ -d "$d" ] && [ "$(ls -A "$d")" ] && {
