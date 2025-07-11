3b3e3b43c4bc856fadc9171afb27b2c60105733c
Luigi Cocconcelli <k-github@rete.us>
2025-07-11 20:02:58 +0200

    update jammy fms

---
+++ distro-jammy/fn_b2_os_resolvconf.sh
@@ -0,0 +1,56 @@
+# ------------------------------------------------------------------------------
+# customize resolv.conf with public dns
+# https://wiki.opennic.org/api/geoip
+# ------------------------------------------------------------------------------
+
+Resolv.classic() {
+	local n t r='/etc/resolv.conf'
+	File.backup "$r"
+
+	# set known public dns
+	t="cloudflare + quad9"
+	n="search .\noptions timeout:2 rotate\n"
+	n+="nameserver 1.1.1.1         # cloudflare\n"
+	n+="nameserver 9.9.9.10        # quad9\n"
+	n+="nameserver 1.0.0.1         # cloudflare\n"
+	n+="nameserver 149.112.112.112 # quad9\n"
+
+	# install needed packages, if missing
+	Pkg.requires e2fsprogs
+
+	# write to /etc/resolv.conf
+	[ -s "$r" ] && cmd chattr -i "$r"	# allow file modification
+	echo -e "# public dns\n$n" > "$r"
+	cmd chattr +i "$r"					# disallow file modification
+
+	Msg.info "Configuration of $t public dns completed! Now $r has:"
+	sed 's|^|> |' < "$r"
+}	# end Resolv.classic
+
+
+Resolv.systemd() {
+	local f='/etc/systemd/resolved.conf.d'
+
+	# if 'dns_servers.conf' already exists, then exit here
+	[ -s "$f/dns_servers.conf" ] && return
+
+	# copying files
+	mkdir -p "$f"
+	File.into "$f" resolved.conf.d/*
+
+	# fully activate systemd-resolved
+	cmd systemctl unmask systemd-resolved
+	cmd systemctl enable systemd-resolved
+	cmd systemctl restart systemd-resolved
+	#cmd systemd-resolve --status
+
+	Msg.info "Configuration of public dns completed via systemd-resolved"
+}	# end Resolv.systemd
+
+
+OS.resolvconf() {
+	# if resolv.conf is a valid symlink, then setup via systemd
+	File.islink '/etc/resolv.conf' \
+		&& Resolv.systemd \
+		|| Resolv.classic
+}	# end OS.resolvconf
---
+++ distro-jammy/fn_u1_menu_fmserver.sh
@@ -0,0 +1,136 @@
+# ------------------------------------------------------------------------------
+# install filemaker server 20 on ubuntu 20.04 focal
+# ------------------------------------------------------------------------------
+
+fms.ssl() {
+	# install letsencrypt ssl certificate
+	local ssl r
+
+	r=/opt/FileMaker	# fm root
+	ln -nfs "$r/FileMaker Server/NginxServer/htdocs/httpsRoot" "$r/webroot"
+
+	# install required packages
+	Pkg.requires cron
+
+	# get cert from letsencrypt
+	curl https://get.acme.sh | bash
+	ssl=~/.acme.sh/acme.sh
+	ssl --set-default-ca --server letsencrypt
+	ssl --register-account -m $LENC_MAIL
+	ssl --issue -d $HOST_FQDN -w "$r/webroot"
+	r="/opt/FileMaker/FileMaker Server/CStore"
+	ssl --install-cert -d $HOST_FQDN \
+	  --key-file       "$r/le-keyfile.key"  \
+	  --fullchain-file "$r/le-fullchain.crt"
+
+	# install cert & restart server
+	cd "$r"
+	fmsadmin certificate import ./le-fullchain.crt \
+	  --keyfile ./le-keyfile.key -u admin -p bamalama -y
+	fmsadmin restart adminserver -u admin -p bamalama -y
+	fmsadmin restart server -u admin -p bamalama -y
+	service fmshelper --full-restart
+}	# end fms.ssl
+Menu.acme()  { fms.ssl "$@"; }	# alias fn
+
+
+fms.firewall() {
+	# post installation cleaning
+
+	# stopping & disabling firewalld
+	Msg.info "Stopping and disabling firewalld..."
+	systemctl stop firewalld
+	systemctl disable firewalld
+
+	# conditional install ufw firewall
+	Pkg.requires ufw
+
+	# configuring ufw
+	Msg.info "Configuring ufw firewall..."
+	ufw allow $SSHD_PORT/tcp
+	ufw allow 5003/tcp
+	ufw allow 2399/tcp
+	ufw allow 443/tcp
+	ufw allow 80/tcp
+	ufw --force enable
+	ufw status verbose
+
+	# add symlink to databases folder in root home
+	Msg.info "Creating symlink to databases folder in home..."
+	ln -nfs "/opt/FileMaker/FileMaker Server/Data/Databases" ~/fmdb
+}	# end fms.firewall
+
+
+fms.download() {
+	# download using wget with progress feedback & resume support
+	# $1 -> URL
+	# $2 -> destination filename
+	Arg.expect "$1" "$2" || exit
+
+	Pkg.requires wget
+	wget -c --quiet --progress=bar:force:noscroll --no-check-certificate "$1" -O "$2" || {
+		Msg.info "Download failed ( $2 ), exiting..."
+		exit
+	}
+} # end fms.download
+
+
+fms.install() {
+	# filemaker server full installation
+	local u="$1" d=$(mktemp -d)	# temporary folder
+
+	# conditional install required packages
+	Pkg.requires unzip wget libxt6
+
+	# download & unzip filemaker server
+	Msg.info "Downloading FileMaker Server..."
+	cd "$d"
+	fms.download "$u" fms.zip
+	unzip fms.zip
+
+	# populating "Assisted Install"
+	cat > "Assisted Install.txt" <<- EOF
+		[Assisted Install]
+		License Accepted=1
+		Deployment Options=0
+		Admin Console User=admin
+		Admin Console Password=bamalama
+		Admin Console PIN=9417
+		License Certificate Path=
+		Filter Databases=0
+		Remove Desktop Shortcut=0
+		Remove Sample Database=0
+		EOF
+
+	# do the real installation
+	Msg.info "Installing FileMaker Server..."
+	apt update
+	FM_ASSISTED_INSTALL="$d" apt -y install ./filemaker-*.deb
+
+	# install microsoft fonts, preseeding license
+	Msg.info "Installing Microsoft fonts..."
+	debconf-set-selections <<- EOF
+		msttcorefonts msttcorefonts/accepted-mscorefonts-eula boolean true
+		EOF
+	apt -y install ttf-mscorefonts-installer
+}	# end fms.install
+
+
+Menu.fms() {
+	# install filemaker server
+	local v d u
+#	v="21.1.5.500 XPnmKS6pm7RBZEk"	# version + nextcloud key
+	v="22.0.1.66 BRGz9WdjrwzCr3M"	# version + nextcloud key
+	d=/opt/FileMaker		# directory root
+	u="https://cloud.inoc.in/s/${v#* }/download/fms_${v%% *}_Ubuntu22_amd64.zip"
+
+	# test if filemaker server is already installed
+	[ -d "$d" ] && [ "$(ls -A "$d")" ] && {
+		Msg.warn "FileMaker Server ${v%% *} is already installed in ${d}..."
+		return
+	}
+
+	fms.install "$u"
+	fms.firewall
+	cd
+}	# end Menu.fms
