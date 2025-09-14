1169c026009f32d5970a0fc15233f0b5958ae882
Luigi Cocconcelli <k-github@rete.us>
2025-09-14 10:52:57 +0200

    update d12 bookworm

--- distro-bookworm/fn_b4_os_minimalize.sh
+++ distro-bookworm/fn_b4_os_minimalize.sh
@@ -2,52 +2,19 @@
 # customize the OS, minimalizing the installed packages
 # ------------------------------------------------------------------------------
 
-Min.prepare() {
-	# always use --no-install-recommends (also used as a check in "Deps.performed")
-	cat > /etc/apt/apt.conf.d/99norecommend <<- EOF
-		APT::Install-Recommends "0";
-		APT::Install-Suggests "0";
-		EOF
+Min.finishing() {
+	# this fn is particular for ubuntu 22 jammy
+	Msg.info "Finalizing minimize procedure for $ENV_os $ENV_arch"
 
-	# avoid interactive prompts preseeding libc6 & postfix via debconf-set-selections
-	debconf-set-selections <<- EOF
-		libc6 libraries/restart-without-asking boolean true
-		libc6:amd64 libraries/restart-without-asking boolean true
-		postfix postfix/main_mailer_type select Internet Site
-		postfix postfix/mailname string $MAIL_NAME
-		postfix postfix/destinations string $HOST_FQDN,localhost
-		EOF
+	# re purge hards to die
+#	apt purge mailcap media-types
+	# re install wrongly purged
+	apt install curl figlet iotop htop rsync screen ufw
 
-	# purge foreign architectures (i*86, etc) fully, removing arch from dpkg
-	local a
-	for a in $(dpkg --print-foreign-architectures); do
-		apt-get purge -qqy ".*:$a"			# purge packages of foreign arch
-		dpkg --remove-architecture "$a"		# remove architecture from dpkg
-		Msg.info "Purged architecture '$a' completed"
-	done;
-}	# end Min.prepare
-
-
-Min.reset() {
-	Pkg.update	# update packages lists
-
-	# merge infos on availabes packages
-	apt-cache dumpavail | dpkg --merge-avail			# from jessie onward
-	[ -d /var/lib/dpkg ] && rm -rf /var/lib/dpkg/*-old	# clean old dpkg metadata files
-
-	# feed packages with priority: required, important
-	dpkg --clear-selections
-	dpkg-query -Wf '${Package} ${Priority}\n' | awk '$2~/ed$|nt$/{print $1,"install"}' | dpkg --set-selections
-	# on full virtualization keep kernel, initramfs and grub packages
-	dpkg-query -Wf '${Package} ${Priority}\n' | awk '$1~/^grub|^initram|^linux/{print $1,"install"}' | dpkg --set-selections
-
-	# add custom packages from file (custom minimal needed packages)
-	local p=$( File.path pkgs.custom.txt )
-	[ -s "$p" ] && dpkg --set-selections < "$p"
-
-	# purge all packages marked as deinstall or purge
-	dpkg --get-selections | awk '$2 ~ /e/ {print $1,"purge"}' | dpkg --set-selections
-}	# end Min.reset
+	apt-get -qy dist-upgrade	# ends performing dist-upgrade
+	apt autoremove
+	dpkg --get-selections > ~/selections.txt
+}	# end Min.finishing
 
 
 Min.loop() {
@@ -74,19 +41,52 @@ Min.loop() {
 }	# end Min.loop
 
 
-Min.finishing() {
-	# this fn is particular for ubuntu 22 jammy
-	Msg.info "Finalizing minimize procedure for $ENV_os $ENV_arch"
+Min.reset() {
+	Pkg.update	# update packages lists
 
-	# re purge hards to die
-	apt purge mailcap media-types
-	# re install wrongly purged
-	apt install curl iotop htop rsync screen telnet ufw
+	# merge infos on availabes packages
+	apt-cache dumpavail | dpkg --merge-avail			# from jessie onward
+	[ -d /var/lib/dpkg ] && rm -rf /var/lib/dpkg/*-old	# clean old dpkg metadata files
 
-	apt-get -qy dist-upgrade	# ends performing dist-upgrade
-	apt autoremove
-	dpkg --get-selections > ~/selections.txt
-}	# end Min.finishing
+	# feed packages with priority: required, important
+	dpkg --clear-selections
+	dpkg-query -Wf '${Package} ${Priority}\n' | awk '$2~/ed$|nt$/{print $1,"install"}' | dpkg --set-selections
+	# on full virtualization keep kernel, initramfs and grub packages
+	dpkg-query -Wf '${Package} ${Priority}\n' | awk '$1~/^grub|^initram|^linux/{print $1,"install"}' | dpkg --set-selections
+
+	# add custom packages from file (custom minimal needed packages)
+	local p=$( File.path pkgs.custom.txt )
+	[ -s "$p" ] && dpkg --set-selections < "$p"
+
+	# purge all packages marked as deinstall or purge
+	dpkg --get-selections | awk '$2 ~ /e/ {print $1,"purge"}' | dpkg --set-selections
+}	# end Min.reset
+
+
+Min.prepare() {
+	# always use --no-install-recommends (also used as a check in "Deps.performed")
+	cat > /etc/apt/apt.conf.d/99norecommend <<- EOF
+		APT::Install-Recommends "0";
+		APT::Install-Suggests "0";
+		EOF
+
+	# avoid interactive prompts preseeding libc6 & postfix via debconf-set-selections
+	debconf-set-selections <<- EOF
+		libc6 libraries/restart-without-asking boolean true
+		libc6:amd64 libraries/restart-without-asking boolean true
+		postfix postfix/main_mailer_type select Internet Site
+		postfix postfix/mailname string $MAIL_NAME
+		postfix postfix/destinations string $HOST_FQDN,localhost
+		EOF
+
+	# purge foreign architectures (i*86, etc) fully, removing arch from dpkg
+	local a
+	for a in $(dpkg --print-foreign-architectures); do
+		apt-get purge -qqy ".*:$a"			# purge packages of foreign arch
+		dpkg --remove-architecture "$a"		# remove architecture from dpkg
+		Msg.info "Purged architecture '$a' completed"
+	done;
+}	# end Min.prepare
 
 
 OS.minimalize() {
